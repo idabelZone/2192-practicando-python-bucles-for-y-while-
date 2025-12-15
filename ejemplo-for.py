8bb16b7b0b3d632f153ba08a5db608e74d468bb7
idabel coparropa <idabelcoparropa382@gmail.com>
2025-12-15 12:06:20 -0500

    Correct length calculation and enhance comments
    Fix the length calculation and improve comments.

--- ejemplo-for.py
+++ ejemplo-for.py
@@ -1,4 +1,5 @@
 """ nombres = ['Carlos', 'Maria', 'Isabela', 'Luis']
+##Supongamos que se quiere exhibir todos los nombres que hay en esta lista, se quiere literar sobre ella
 
 numeroNombres = len(nombres)
 print(numeroNombres,"numeroNombres")
@@ -10,13 +11,14 @@ for nombre in nombres:
     print(n) """
 
 """ nombres = ['Carlos', 'Maria', 'Isabela', 'Luis']
-cantidadNombres = len(nombres)
+cantidadNombres = len ## esta funcion se utiliza para hacer la lista en si y colocar la cantidad que hay en la lista(nombres)
 print(cantidadNombres, 'numeros')
 
 
-for n in range(5):
+for n in range(5) ##la cantidad de veces que se realiza desde 0 -antes de llegar al número:
     print(n)
  """
+#Shift + Alt + A es para hacer comentario dentro de un código de python
 
 nombres = ['Carlos', 'Maria', 'Isabela', 'Luis']
 cantidadNombres = len(nombres)
