8360238bf093822374892ae12c26edebf874c324
idabel coparropa <idabelcoparropa382@gmail.com>
2025-12-15 11:02:00 -0500

    Add comparison operators example in Python

---
+++ operadores-comparacion.py
@@ -0,0 +1,15 @@
+""" Operadores comparación
+
+== igual
+>  más grande
+>= más grande o igual
+< más pequeno
+<= más pequeño o igual
+ """
+
+numero = 11
+
+if numero != 12:
+    print("más grande")
+else:
+    print("más pequeño")
