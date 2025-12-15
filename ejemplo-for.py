21cff777add4f587bae30ba6a549993129ad69c9
idabel coparropa <idabelcoparropa382@gmail.com>
2025-12-15 11:00:32 -0500

    Implement for loop examples in ejemplo-for.py
    Added example of using for loops with lists and ranges.

---
+++ ejemplo-for.py
@@ -0,0 +1,26 @@
+""" nombres = ['Carlos', 'Maria', 'Isabela', 'Luis']
+
+numeroNombres = len(nombres)
+print(numeroNombres,"numeroNombres")
+
+for nombre in nombres:
+    print("Hola" + nombre)  """
+
+""" for n in range(4):
+    print(n) """
+
+""" nombres = ['Carlos', 'Maria', 'Isabela', 'Luis']
+cantidadNombres = len(nombres)
+print(cantidadNombres, 'numeros')
+
+
+for n in range(5):
+    print(n)
+ """
+
+nombres = ['Carlos', 'Maria', 'Isabela', 'Luis']
+cantidadNombres = len(nombres)
+print(cantidadNombres)
+
+for n in range(7):
+    print(n)
