c64a4b17a1fb4c84370ce9595a1c1bf33241ceb9
idabel coparropa <idabelcoparropa382@gmail.com>
2025-12-15 11:01:15 -0500

    Add multiple while loop examples in ejemplo-while.py

---
+++ ejemplo-while.py
@@ -0,0 +1,29 @@
+""" count = 1
+
+while(count < 10):
+    print("Estoy acá", count)
+    count += 1
+ """
+
+""" count = 0
+
+while (count < 10):
+    count = count + 1
+    if count == 3:
+        continue
+    print('continuemos',count)
+ """
+
+""" count = 0
+
+while( count < 10):
+    count = count + 1
+    print(count) """
+
+count = 0
+
+while( count < 10):
+    count = count + 1
+    if count == 5:
+     continue
+    print(count)
